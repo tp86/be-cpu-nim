0de958b44d0db668042597b54215e0625e72608f
tp86 <tom0palka+tp86@gmail.com>
2022-04-06 21:04:55 +0200

    Merge connection and gate modules

--- src/gate.nim
+++ src/gate.nim
@@ -1,37 +1,89 @@
-import std/[sequtils, tables]
-import connection
+import std/sequtils
 import signal
 
 type
+  Input* = ref object
+    parent: Gate
+    sig: Signal
+    connected: bool
+  Output* = ref object
+    connections: seq[Input]
+    lastSignal: Signal
+  ConnectionError* = object of CatchableError
+
   GateKind = enum
-    gNot
+    gSource
+    gSink
+    gIn1
   Gate* = ref object
     case kind: GateKind
-    of gNot:
-      a: Input
-      b: Output
+    of gSource:
+      signal: proc (): Signal
+      output*: Output
+    of gSink:
+      input*: Input
+    of gIn1:
+      signal1: proc (s: Signal): Signal
+      a1: Input
+      b1: Output
   IO = Input or Output
 
-var
-  inputToGate = initTable[Input, Gate]()
+using
+  i: Input
+  o: Output
+  s: Signal
+  g: Gate
 
-proc gates(inputs: seq[Input]): seq[Gate] =
-  inputs.mapIt(inputToGate.getOrDefault(it))
+proc newInput(g): Input = Input(parent: g)
 
-proc update*(g: Gate): seq[Gate] =
+proc signal*(i): Signal = i.sig
+
+proc newOutput(): Output = Output()
+
+proc `~~`*(o, i) =
+  if i.connected:
+    raise newException(ConnectionError, "Cannot connect multiple times to the same input")
+  o.connections.add i
+  i.connected = true
+
+proc propagate(o, s): seq[Gate] =
+  result = o.connections.mapIt(it.parent)
+  # depends on input's default signal
+  if s == o.lastSignal:
+    return @[]
+  o.lastSignal = s
+  for input in o.connections:
+    input.sig = s
+
+proc A*(g): IO =
   case g.kind
-  of gNot:
-    let s = !g.a.signal
-    g.b.propagate(s).gates
+  of gIn1: g.a1
+  else: nil
 
-proc A*(g: Gate): IO =
+proc B*(g): IO =
   case g.kind
-  of gNot: g.a
+  of gIn1: g.b1
+  else: nil
 
-proc B*(g: Gate): IO =
+proc update*(g): seq[Gate] =
   case g.kind
-  of gNot: g.b
+  of gSource:
+    g.output.propagate(g.signal())
+  of gSink: @[]
+  of gIn1:
+    let s = g.signal1(g.a1.signal)
+    g.b1.propagate(s)
+
+proc newSource*(signal: proc (): Signal): Gate =
+  Gate(kind: gSource, signal: signal, output: newOutput())
+
+proc newConstantSource*(s = L): Gate =
+  Gate(kind: gSource, signal: proc (): Signal = s, output: newOutput())
+
+proc newSink*(): Gate =
+  result = Gate(kind: gSink)
+  result.input = newInput(result)
 
 proc newNot*(): Gate =
-  result = Gate(kind: gNot, a: newInput(), b: newOutput())
-  inputToGate[result.a] = result
+  result = Gate(kind: gIn1, signal1: `!`, b1: newOutput())
+  result.a1 = newInput(result)
--- tests/gate_test.nim
+++ tests/gate_test.nim
@@ -1,7 +1,95 @@
-import std/[sequtils, unittest]
-import connection
+import std/unittest
 import signal
 import gate
+
+suite "gate connections":
+
+  setup:
+    let
+      source = newConstantSource()
+      sink = newSink()
+
+  test "gates can be connected":
+    source.output ~~ sink.input
+
+  test "gate input may be connected to only once":
+    let source2 = newConstantSource()
+    source.output ~~ sink.input
+    expect(ConnectionError):
+      source2.output ~~ sink.input
+
+  test "gate output may be connected to multiple inputs":
+    let sink2 = newSink()
+    source.output ~~ sink.input
+    source.output ~~ sink2.input
+
+suite "gate signal propagation":
+
+  test "default (unconnected) input signal value is low":
+    let sink = newSink()
+    check sink.input.signal == L
+
+  test "source gate propagates signal change on update":
+    var signal = L
+    let
+      source = newSource(proc(): Signal = signal)
+      sink1 = newSink()
+      sink2 = newSink()
+
+    check sink1.input.signal == L
+    source.output ~~ sink1.input
+    discard source.update
+    check sink1.input.signal == L
+    signal = H
+    discard source.update
+    check sink1.input.signal == H
+    source.output ~~ sink2.input
+    # not propagated on connection
+    check sink2.input.signal == L
+    discard source.update
+    # not propagated since signal hasn't changed
+    check sink2.input.signal == L
+    signal = L
+    discard source.update
+    check sink2.input.signal == L
+    signal = H
+    discard source.update
+    check sink2.input.signal == H
+
+  test "source gate propagates signal on update to all connected inputs":
+    let
+      source = newConstantSource(H)
+      sink1 = newSink()
+      sink2 = newSink()
+
+    source.output ~~ sink1.input
+    source.output ~~ sink2.input
+    check:
+      sink1.input.signal == L
+      sink2.input.signal == L
+    discard source.update
+    check:
+      sink1.input.signal == H
+      sink2.input.signal == H
+
+  test "source gate returns connected gates on changed signal propagation":
+    let
+      source = newConstantSource(H)
+      sink1 = newSink()
+      sink2 = newSink()
+
+    source.output ~~ sink1.input
+    source.output ~~ sink2.input
+    let gates = source.update
+    check gates == @[sink1, sink2]
+
+  test "gate does not propagate signal on connection":
+    let
+      source = newConstantSource(H)
+      sink = newSink()
+
+    source.output ~~ sink.input
+    check sink.input.signal == L
 
 suite "gates":
 
@@ -9,18 +97,24 @@ suite "gates":
     let notGate = newNot()
     check:
       typeOf(notGate.A) is Input
+      notGate.A != nil
       typeOf(notGate.B) is Output
+      notGate.B != nil
 
   test "not gate logic":
-    let notGate = newNot()
-    let source = newOutput()
-    let probe = newInput()
-    source.connect(notGate.A)
-    notGate.B.connect(probe)
-    discard source.propagate(L)
-    discard notGate.update
-    check probe.signal == H
-    discard source.propagate(H)
-    discard notGate.update
-    check probe.signal == L
+    var signal: Signal
+    let
+      source = newSource(proc(): Signal = signal)
+      notGate = newNot()
+      sink = newSink()
 
+    source.output ~~ notGate.A
+    notGate.B ~~ sink.input
+    signal = L
+    discard source.update
+    discard notGate.update
+    check sink.input.signal == H
+    signal = H
+    discard source.update
+    discard notGate.update
+    check sink.input.signal == L

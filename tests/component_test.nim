c107a3fc20dfd528ec46ec224bc9f219215d4f91
tp86 <tom0palka+tp86@gmail.com>
2022-04-07 15:39:53 +0200

    SR latch logic test

--- tests/component_test.nim
+++ tests/component_test.nim
@@ -30,3 +30,36 @@ suite "SR latch":
     check:
       sinkQ.input.signal == L
       sinkQ̅.input.signal == H
+
+  test "logic":
+    var
+      signals = (S: L, R: L)
+    let
+      sourceS = Source(proc(): Signal = signals.S)
+      sourceR = Source(proc(): Signal = signals.R)
+      sinkQ = Sink()
+      sinkQ̅ = Sink()
+
+    sourceS.output ~~ sr.S
+    sourceR.output ~~ sr.R
+    sr.Q ~~ sinkQ.input
+    sr.Q̅ ~~ sinkQ̅.input
+
+    let cases = [
+      (S: L, R: L, Q: L, Q̅: H),
+      (S: L, R: H, Q: L, Q̅: H),
+      (S: L, R: L, Q: L, Q̅: H),
+      (S: H, R: L, Q: H, Q̅: L),
+      (S: L, R: L, Q: H, Q̅: L),
+      (S: H, R: L, Q: H, Q̅: L),
+      (S: L, R: L, Q: H, Q̅: L),
+      (S: L, R: H, Q: L, Q̅: H),
+      (S: L, R: L, Q: L, Q̅: H),
+    ]
+    for c in cases:
+      signals.S = c.S
+      signals.R = c.R
+      updateAll sourceS, sourceR
+      check:
+        sinkQ.input.signal == c.Q
+        sinkQ̅.input.signal == c.Q̅

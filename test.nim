8293a7fbbae4093b035fb46c35123cd8cd83e285
tp86 <tom0palka+tp86@gmail.com>
2022-04-07 13:44:27 +0200

    Getters generating macro

--- test.nim
+++ test.nim
@@ -1,11 +1,29 @@
 import std/macros
 
 type
-  X = ref object
+  X = ref object of RootObj
     a, b: int
+    c: string
+    d: array[1, bool]
+
 
 # generate getters for fields of X
 macro getters(T: untyped) =
-  echo T.getImpl.treerepr
+  var t = T.getImpl[2]
+  if t.kind == nnkRefTy:
+    t = t[0]
+  result = nnkStmtList.newTree
+  for defs in t[2]:
+    for field in defs[0..^3]:
+      let arg = nskParam.genSym
+      let retType = defs[^2]
+      result.add quote do:
+        proc `field`*(`arg`: `T`): `retType` = `arg`.`field`
 
 X.getters
+
+let x = X(a: 1, b: 2, c: "abc", d: [true])
+echo a(x)
+echo b(x)
+echo c(x)
+echo d(x)
